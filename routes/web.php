21cf46c5dccfd7395f544de0c5a763a8e983faa5
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2026-09-02 19:08:06 -0300

    feat: update PHPStan configuration to include routes directory and clean up API route definition

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use FastRoute\RouteCollector;
 
 return function (RouteCollector $router) {
-    $router->addGroup('/api/v1', function (RouteCollector $router) {
-        // Define your API routes here
-    });
+    $router->addGroup('/api/v1', function (RouteCollector $router) {});
 };
