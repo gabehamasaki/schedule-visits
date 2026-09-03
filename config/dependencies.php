73d629efcdb0eafb6fa4df42e5a09684212b40de
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2026-09-03 16:09:10 -0300

    chore: register use cases in DI container

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Application\UseCases\GetAllVehiclesUseCase;
+use App\Application\UseCases\GetAvailableHoursUseCase;
+use App\Application\UseCases\GetVehicleUseCase;
+use App\Application\UseCases\ScheduleVisitUseCase;
 use App\Domain\Repositories\AppointmentRepositoryInterface;
 use App\Domain\Repositories\VehicleRepositoryInterface;
 use App\Infrastructure\Database\PdoConnection;
@@ -11,6 +15,12 @@ return [
     PDO::class => function (ContainerInterface $container) {
         return PdoConnection::getInstance();
     },
+
+    // Use Cases
+    GetVehicleUseCase::class => \DI\autowire(GetVehicleUseCase::class),
+    GetAllVehiclesUseCase::class => \DI\autowire(GetAllVehiclesUseCase::class),
+    GetAvailableHoursUseCase::class => \DI\autowire(GetAvailableHoursUseCase::class),
+    ScheduleVisitUseCase::class => \DI\autowire(ScheduleVisitUseCase::class),
 
     AppointmentRepositoryInterface::class => \DI\autowire(PdoAppointmentRepository::class),
     VehicleRepositoryInterface::class => \DI\autowire(PdoVehicleRepository::class),
